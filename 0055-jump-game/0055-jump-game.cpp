e931f33594057e62ee981ae6e9d3cf29e71e2344
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-07 02:58:12 +0530

    Time: 46 ms (51.63%), Space: 50.9 MB (23.22%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size()<=1) return true;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i]==0){
+                int x=i;
+                int j=0;
+                while(j-nums[i]>=0 && i>=0){
+                    if(i==0) return false;
+                    j++;
+                    i--;
+                }
+            }
+        }
+        return true;
+    }
+};
