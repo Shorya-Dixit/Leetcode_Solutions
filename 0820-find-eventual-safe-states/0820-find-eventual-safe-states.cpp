a74274b6e500f13e6129af14b2a927902d31f0b7
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-10 14:02:12 +0530

    Time: 152 ms (23.64%), Space: 65.3 MB (15.28%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph,int node,vector<int>&vis,vector<int>&pathvis,vector<int>&check){
-        vis[node]=1;
-        pathvis[node]=1;
-        for(auto it:graph[node]){
-            if(!vis[it]){
-                if(dfs(graph,it,vis,pathvis,check)) {check[node]=0;return true;}
-            }
-            else if(pathvis[it]) {check[node]=0;return true;}
-        }
-        check[node]=1;
-        pathvis[node]=0;
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int V=graph.size();
-        vector<int>vis(V,0);
-        vector<int>pathvis(V,0);
-        vector<int>check(V,0);
-        vector<int>ans;
-        for(int i=0;i<V;i++){
-            if(!vis[i]) dfs(graph,i,vis,pathvis,check);
-        }
-        for(int i=0;i<V;i++){
-            if(check[i]) ans.push_back(i);
-        }
-        return ans;
+        vector<vector<int>>revGraph(V);
+        vector<int>indegree(V,0);
+		for (int i = 0; i < V; i++) {
+			for (auto it : graph[i]) {
+                revGraph[it].push_back(i);
+				indegree[i]++;
+			}
+		}
+
+		queue<int> q;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+		vector<int> safeNodes;
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			safeNodes.push_back(node);
+			// node is in your topo sort
+			// so please remove it from the indegree
+
+			for (auto it : revGraph[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0) q.push(it);
+			}
+		}
+        sort(safeNodes.begin(),safeNodes.end());
+		return safeNodes;
     }
 };
 //if a node is a part of a cicle, its not safe
+//check last submission too.
