9c53343e0b059aeb91551e847ddeef8118a37257
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-13 14:50:01 +0530

    Time: 8 ms (65.97%), Space: 21.7 MB (30.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int target, int result, int ind){
-        if(ind==nums.size()){
-            if(result==target) return 1;
+    int count(vector<int>& arr,int tsum, int s1,int d, int i,vector<vector<int>>&dp){
+        if(i<0){
+            if(2*s1==tsum+d) return 1;
             else return 0;
-        } 
-        int add=f(nums,target,result+nums[ind],ind+1);
-        int sub=f(nums,target,result-nums[ind],ind+1);
-        return add+sub;
+        }
+        if(dp[i][s1]!=-1) return dp[i][s1];
+        int nottake=count(arr,tsum,s1,d,i-1,dp);
+        int take=0;
+        if(2*(arr[i]+s1)<=tsum+d) take=count(arr,tsum,s1+arr[i],d,i-1,dp);
+        return dp[i][s1]=(nottake+take);
+    }
+    int countPartitions(int n, int d, vector<int>& arr) {
+        int tsum=0;
+        for(auto i:arr) tsum+=i;
+        vector<vector<int>>dp(n+1,vector<int>((tsum+d)/2+1,-1));
+        return count(arr,tsum,0,d,n-1,dp);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(nums,target,0,0);
+        int tsum=0;
+        int n=nums.size();
+        for(auto i:nums) tsum+=i;
+        vector<vector<int>>dp(n+1,vector<int>((tsum+1000)/2+1,-1));//max limit of target is 1000
+        return count(nums,tsum,0,target,n-1,dp);
     }
 };
-// we wont be able to memoize it as to do it we have to declare a dp[ind][result]
-// but result can also be negative at somepoint before the final result(positive)
+// exactly same as "Count partitions with given difference" problem.
+// check previous submission too
