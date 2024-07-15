2a8d9abc8569c0707d6a87c1aa99c312fe6000be
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-16 02:56:15 +0530

    Time: 7 ms (12.08%), Space: 11.4 MB (5.68%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -9,10 +9,9 @@ public:
         while(left<=right)
         {
             mid=(left+right)/2;
-            if(nums[mid]<nums[mid+1] && nums[mid]>nums[mid-1] ) left=mid+1;
-            else if(nums[mid]>nums[mid+1] && nums[mid]<nums[mid-1]) right=mid-1;
-            else if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) return mid;
-            else right= mid-1;
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]) return mid;
+            else if(nums[mid]>nums[mid-1] ) left=mid+1;
+            else right=mid-1;
         }
         return -1;
     }
