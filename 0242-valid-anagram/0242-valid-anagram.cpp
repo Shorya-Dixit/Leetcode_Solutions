c6c6db0fabaae9da0eb55579917ac561d4e3417f
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-16 22:10:09 +0530

    Time: 8 ms (63.83%), Space: 8.9 MB (41.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,20 @@
 class Solution {
-public: 
+public:
     bool isAnagram(string str1, string str2)
     {
-        // Case 1: when both of the strings have different lengths
-        if (str1.length() != str2.length())
-            return false;
-        
-        sort(str1.begin(), str1.end());
-        sort(str2.begin(), str2.end());
-        
-        // Case 2: check if every character of str1 and str2 matches with each other
+        if (str1.length() != str2.length()) return false;     
+        int freq[26] = {0};
         for (int i = 0; i < str1.length(); i++)
         {
-            if (str1[i] != str2[i])
+            freq[str1[i] - 'a']++;
+        }
+        for (int i = 0; i < str2.length(); i++)
+        {
+            freq[str2[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (freq[i] != 0)
             return false;
         }
         return true;
