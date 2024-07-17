39e73571bdb8025701e001529887caa5c8d4b240
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-17 22:13:31 +0530

    Time: 134 ms (37.05%), Space: 26.8 MB (33.5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    string longestPalindrome(std::string s) {
+    string longestPalindrome(string s) {
         if (s.length() <= 1) {
             return s;
-        }
-
-        auto expand_from_center = [&](int left, int right) {
-            while (left >= 0 && right < s.length() && s[left] == s[right]) {
-                left--;
-                right++;
+        }       
+        int max_len = 1;
+        int start = 0;
+        int end = 0;
+        vector<vector<bool>> dp(s.length(),vector<bool>(s.length(), false));      
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > max_len) {
+                        max_len = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
             }
-            return s.substr(left + 1, right - left - 1);
-        };
-
-        string max_str = s.substr(0, 1);
-
-        for (int i = 0; i < s.length() - 1; i++) {
-            string odd = expand_from_center(i, i);
-            string even = expand_from_center(i, i + 1);
-
-            if (odd.length() > max_str.length()) {
-                max_str = odd;
-            }
-            if (even.length() > max_str.length()) {
-                max_str = even;
-            }
-        }
-
-        return max_str;
+        }       
+        return s.substr(start, end - start + 1);
     }
 };
-// Time complexity : O(n^2). expanding a palindrome around its center could take O(n) time, 
-// Space complexity : O(1).
+// Time complexity : O(n^2). 
+// Space complexity : O(n^2). 
