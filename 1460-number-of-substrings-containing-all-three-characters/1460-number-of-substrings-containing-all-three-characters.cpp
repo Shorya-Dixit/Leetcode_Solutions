e374616cf53f51070106eaf7278c535448ac7a4f
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-19 03:39:20 +0530

    Time: 20 ms (45.88%), Space: 10 MB (38.6%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-        unordered_map<char,int>mp;
-        int count=0;
-        int left=0;
-        int right=0;  
-        while(right<s.size()){
-            mp[s[right]]++;
-            while(mp['a']>0 && mp['b']>0 && mp['c']>0){
-                count+=s.size()-right;
-                mp[s[left]]--;
-                left++;
+    int numberOfSubstrings(string s) {     
+       int arr[3] = {-1, -1, -1}; 
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            arr[s[i] - 'a'] = i; 
+            if (arr[0] != -1 && arr[1] != -1 && arr[2] != -1) {
+                count += 1 + min(arr[0], min(arr[1], arr[2]));
             }
-            right++;
         }
         return count;
     }
 };
+//check last submission too
