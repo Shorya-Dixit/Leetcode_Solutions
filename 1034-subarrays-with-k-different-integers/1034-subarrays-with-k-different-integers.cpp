649fa4e57ee4a0c9b58370061b9bbf3dabb8bdd8
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-22 01:26:57 +0530

    Time: 121 ms (71.12%), Space: 56.2 MB (38.53%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,43 +1,23 @@
 class Solution {
    public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        // Array to store the count of distinct values encountered
-        vector<int> distinctCount(nums.size() + 1, 0);
-
-        int totalCount = 0;
-        int left = 0;
-        int right = 0;
-        int currCount = 0;
-
-        while (right < nums.size()) {
-            // Increment the count of the current element in the window
-            if (++distinctCount[nums[right++]] == 1) {
-                // If encountering a new distinct element, decrement K
-                k--;
+    int f(vector<int>& nums,int k){
+        unordered_map<int,int>mp;
+        int left=0;
+        int right=0;
+        int result=0;
+        while(right<nums.size()){
+            mp[nums[right]]++;
+            while(mp.size()>k){
+                mp[nums[left]]--;
+                if (mp[nums[left]] == 0) mp.erase(nums[left]);
+                left++;
             }
-
-            // If K becomes negative, adjust the window from the left
-            if (k < 0) {
-                // Move the left pointer until the count of distinct elements
-                // becomes valid again
-                --distinctCount[nums[left++]];
-                k++;
-                currCount = 0;
-            }
-
-            // If K becomes zero, calculate subarrays
-            if (k == 0) {
-                // While the count of left remains greater than 1, keep
-                // shrinking the window from the left
-                while (distinctCount[nums[left]] > 1) {
-                    --distinctCount[nums[left++]];
-                    currCount++;
-                }
-                // Add the count of subarrays with k distinct elements to the
-                // total count
-                totalCount += (currCount + 1);
-            }
+            result+=right-left+1;
+            right++;
         }
-        return totalCount;
+        return result;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return f(nums,k)-f(nums,k-1);
     }
 };
