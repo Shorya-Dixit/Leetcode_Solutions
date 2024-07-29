f5c798b0a527371f06c498876630077186bb826c
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-07-30 00:55:00 +0530

    Time: 27 ms (50.39%), Space: 26 MB (24.04%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    int findLength(string &text1, string &text2, int i, int j, vector<vector<int>>&dp ){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(text1[i]==text2[j]) return dp[i][j]=(1+findLength(text1,text2,i-1,j-1,dp));
-        else return dp[i][j]=(0+max(findLength(text1,text2,i-1,j,dp),findLength(text1,text2,i,j-1,dp)));
-    }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return findLength(text1,text2,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<m+1;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
+//index shifting is done 1 place towards right. i.e 0 is treated as -1 here.
