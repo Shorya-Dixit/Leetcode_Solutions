70582f349645843221725fd3ea7139772c0d436d
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-08-02 05:41:28 +0530

    Time: 21 ms (47.59%), Space: 31.7 MB (27.9%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int count(string &s, string &t, int i, int j, vector<vector<int>>&dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
-            return dp[i][j]=count(s,t,i-1,j-1,dp)+count(s,t,i-1,j,dp);
-        }
-        else return dp[i][j]=count(s,t,i-1,j,dp);
-    }
     int numDistinct(string s, string t) {
-        vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
-        return count(s,t,s.size()-1,t.size()-1,dp);
+        int n=s.size();
+        int m=t.size();
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;    // only 1 way that a subsequence of s[0 to i] is eq. to NULL string
+        }
+        for(int j=1;j<=m;j++){
+            dp[0][j]=0;    // no way that a subsequence of NULL string is eq. to t[0 to j].
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return (int)dp[n][m];
     }
 };
 // here dp[i][j] represents the no. of distinct subsequences of s[0 to i] which equals t[0 to j]. 
+// we have done index by 1 towards right for tabulation as dp of negative is not possible and 
+// we need to accomodate i<0 and j<0 in our dp table.
