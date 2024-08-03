d20e08053d9ba13f44260d992cc8931e2143834a
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-08-03 14:03:05 +0530

    Time: 60 ms (37.63%), Space: 14.9 MB (77.34%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
     bool isAllStars(string &p, int i) {
-        for (int k = 0; k <= i; k++) {
-            if (p[k] != '*')
+        for (int k = 1; k <= i; k++) {
+            if (p[k-1] != '*')
                 return false;
         }
         return true;
     }
-    bool wildcardMatchingUtil(string &p, string &s, int i, int j, vector<vector<int>> &dp) {
-        // Base Cases
-        if (i < 0 && j < 0)
-            return true;
-        if (i < 0 && j >= 0) 
-            return false;
-        if (j < 0 && i >= 0)
-            return isAllStars(p, i);
-
-        // If the result for this state has already been calculated, return it
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        // If the characters at the current positions match or p has a '?'
-        if (p[i] == s[j] || p[i] == '?')
-            return dp[i][j] = wildcardMatchingUtil(p, s, i - 1, j - 1, dp);
-        else {
-            if (p[i] == '*')
-                // Two options: either '*' represents an empty string or it matches a character in s
-                return dp[i][j] = wildcardMatchingUtil(p, s, i - 1, j, dp) || wildcardMatchingUtil(p, s, i, j - 1, dp);
-            else
-                return false;
-        }
-    }
-
-    // Main function to check if p matches s using wildcard pattern matching
-    bool isMatch(string &s, string &p) {
+    bool isMatch(string s, string p) {
         int n = p.size();
         int m = s.size();
 
         // Create a DP table to memoize results
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return wildcardMatchingUtil(p, s, n - 1, m - 1, dp);
+        vector<vector<bool>> dp(n + 1, vector<bool>(m+1, false));
+
+        // Initialize the first row and column
+        dp[0][0] = true;
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = false;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = isAllStars(p, i);
+        }
+
+        // Fill in the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (p[i - 1] == s[j - 1] || p[i - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } 
+                else {
+                    if (p[i - 1] == '*') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    } 
+                    else {
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
