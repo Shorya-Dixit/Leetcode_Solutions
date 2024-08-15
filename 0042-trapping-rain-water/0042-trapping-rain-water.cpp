0f7f82d103baaa577ce86268de5f4372fa57f8d1
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-08-15 14:18:35 +0530

    Time: 8 ms (75.06%), Space: 24.9 MB (9.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    vector<int> rightHeight(vector<int>& nums) {
-        vector<int>ans(nums.size());
-        int height=0;
-        for(int i=nums.size()-1;i>=0;i--){
-            ans[i]=height;
-            height=max(height,nums[i]);
-        }
-        return ans;
-    }
-    vector<int> leftHeight(vector<int>& nums) {
-        vector<int>ans(nums.size());
-        int height=0;
-        for(int i=0;i<nums.size();i++){
-            ans[i]=height;
-            height=max(height,nums[i]);
-        }
-        return ans;
-    }
     int trap(vector<int>& height) {
-        vector<int>left=leftHeight(height);
-        vector<int>right=rightHeight(height);
-        int area=0;
-        for(int i=0;i<height.size();i++){
-            if(left[i]>height[i] && right[i]>height[i]) area+=min(right[i]-height[i],left[i]-height[i]);
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int res = 0;
+        int maxLeft = 0, maxRight = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+            if (height[left] >= maxLeft) {
+                maxLeft = height[left];
+            } else {
+                res += maxLeft - height[left];
+            }
+            left++;
+            } else {
+            if (height[right] >= maxRight) {
+                maxRight = height[right];
+            } else {
+                res += maxRight - height[right];
+            }
+            right--;
+            }
         }
-        return area;
+        return res;
     }
 };
+// Intuition: We need a minimum of leftMax and rightMax.So if we take the case when 
+// height[l]<=height[r] we increase l++, so we can surely say that there is a block with a height
+// more than height[l] to the right of l. And for the same reason when height[r]<=height[l] 
+// we can surely say that there is a block to the left of r which is at least of height[r]. 
+// TC- O(N), SC- O(1)
+// check previous solution too TC- O(3N), SC- O(2N)
