3ea36f6c597acdd50683f792ad9876eeb236eb3d
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-08-18 16:40:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -0,0 +1,156 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    void markParentsAndTarget(Node* root,Node* previous, unordered_map<Node*,Node*>&parent, int target, Node* &targetNode){
+        if(!root) return;
+        if(root->data==target) targetNode=root; // marking target node
+        parent[root]=previous;
+        markParentsAndTarget(root->left,root,parent,target,targetNode);
+        markParentsAndTarget(root->right,root,parent,target,targetNode);
+        return;
+    }
+    int minTime(Node* root, int target) 
+    {
+        Node* targetNode;
+        unordered_map<Node*,Node*>parent;
+        markParentsAndTarget(root,NULL,parent,target,targetNode);
+        unordered_map<Node*,bool>visited;
+        queue<pair<Node*,int>>q;
+        int minTime=0;
+        q.push({targetNode,0});
+        while(!q.empty()){
+            Node* node=q.front().first;
+            minTime=q.front().second;
+            visited[node]=true;
+            q.pop();
+            if(parent[node] && !visited[parent[node]]) q.push({parent[node],minTime+1});
+            if(node->left && !visited[node->left]) q.push({node->left,minTime+1});
+            if(node->right && !visited[node->right]) q.push({node->right,minTime+1});
+        }
+        return minTime;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) 
+    {    
+        string treeString;
+        getline(cin, treeString);
+        // cout<<treeString<<"\n";
+        int target;
+        cin>>target;
+        // cout<<target<<"\n";
+
+        Node *root = buildTree(treeString);
+        Solution obj;
+        cout<<obj.minTime(root, target)<<"\n"; 
+
+        cin.ignore();
+
+    }
+
+
+    return 0;
+}
+
+// } Driver Code Ends
