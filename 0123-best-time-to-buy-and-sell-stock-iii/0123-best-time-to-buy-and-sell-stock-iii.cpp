d339ff24dccea6d60b432648623ed5d4000e17a7
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-08-24 08:35:17 +0530

    Time: 500 ms (16.64%), Space: 222.4 MB (21.14%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trade(vector<int>& prices, int bought, int i, vector<vector<vector<int>>>&dp, int trans){
+        if(trans==0) return 0;
+        if(i==prices.size()-1 && bought==1) return prices[i];
+        else if(i==prices.size()-1 && bought==0) return 0;
+
+        if(dp[i][bought][trans]!=-1) return dp[i][bought][trans];
+
+        if(!bought) {
+            int buy=trade(prices,1,i+1,dp,trans)-prices[i];
+            int notBuy=trade(prices,0,i+1,dp,trans);
+            return dp[i][bought][trans]=max(buy,notBuy);
+        }
+        else {
+            int sell=trade(prices,0,i+1,dp,trans-1)+prices[i];
+            int notSell=trade(prices,1,i+1,dp,trans);
+            return dp[i][bought][trans]=max(sell,notSell);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<vector<int>>>dp(prices.size(),vector<vector<int>>(2,vector<int>(3,-1)));
+        int trans=2;
+        return trade(prices,0,0,dp,trans);
+    }
+
+};
