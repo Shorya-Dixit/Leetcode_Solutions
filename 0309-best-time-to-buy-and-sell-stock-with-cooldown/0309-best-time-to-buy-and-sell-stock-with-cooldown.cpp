599ae110d1ad1ac582ea15a29cceb688f08cbefd
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-08-24 16:40:12 +0530

    Time: 3 ms (72.89%), Space: 14.8 MB (26.21%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
 // slight modification in base case
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>dp(n+2,vector<int>(2,0));
-        dp[n][0]=0;
-        dp[n][1]=0;
-        for(int i=n-1;i>=0;i--){
-            dp[i][0]=max(dp[i+1][1]-prices[i],dp[i+1][0]);
-            dp[i][1]=max(dp[i+2][0]+prices[i],dp[i+1][1]);
+    int trade(vector<int>& prices, int bought, int i, vector<vector<int>>&dp, int prevSold){
+        if(i>=prices.size()) return 0;
+
+        if(dp[i][bought]!=-1) return dp[i][bought];
+
+        if(!bought) {
+            int buy=trade(prices,1,i+1,dp,0)-prices[i];
+            int notBuy=trade(prices,0,i+1,dp,0);
+            return dp[i][bought]=max(buy,notBuy);
         }
-        return dp[0][0];
+        else {
+            if(prevSold==1) trade(prices,1,i+1,dp,0);
+            int sell=trade(prices,0,i+2,dp,1)+prices[i]; // if sold, leave the just next stock
+            int notSell=trade(prices,1,i+1,dp,0);
+            return dp[i][bought]=max(sell,notSell);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
+        return trade(prices,0,0,dp,0);
     }
 };
 // check all previous submissions
