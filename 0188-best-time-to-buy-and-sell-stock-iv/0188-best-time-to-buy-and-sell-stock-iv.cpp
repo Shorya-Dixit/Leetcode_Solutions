142b79359a9ea2a656d4c6aba747e7dc20601d45
Shorya-Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-08-24 16:11:04 +0530

    Time: 4 ms (88.71%), Space: 16.9 MB (34.79%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int trade(vector<int>& prices, int bought, int i, vector<vector<vector<int>>>&dp, int trans){
-        if(trans==0) return 0;
-        if(i==prices.size()-1 && bought==1) return prices[i];
-        else if(i==prices.size()-1 && bought==0) return 0;
-
-        if(dp[i][bought][trans]!=-1) return dp[i][bought][trans];
-
-        if(!bought) {
-            int buy=trade(prices,1,i+1,dp,trans)-prices[i];
-            int notBuy=trade(prices,0,i+1,dp,trans);
-            return dp[i][bought][trans]=max(buy,notBuy);
-        }
-        else {
-            int sell=trade(prices,0,i+1,dp,trans-1)+prices[i];
-            int notSell=trade(prices,1,i+1,dp,trans);
-            return dp[i][bought][trans]=max(sell,notSell);
-        }
-    }
     int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>>dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return trade(prices,0,0,dp,k);
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        for(int i=0;i<n;i++){
+            dp[i][0][0]=0;
+            dp[i][1][0]=0;
+        }
+        for(int t=1;t<=k;t++){
+            dp[n-1][0][t]=0;
+            dp[n-1][1][t]=prices[n-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int t=1;t<=k;t++){
+                dp[i][0][t]=max(dp[i+1][1][t]-prices[i],dp[i+1][0][t]);
+                dp[i][1][t]=max(dp[i+1][0][t-1]+prices[i],dp[i+1][1][t]);
+            }
+        }
+        return dp[0][0][k];
     }
 };
+// dp[n-1][0][t] means the change in profit if we start from n-1 and haven't bought the stock before.
+// dp[n-1][1][t] means the change in profit if we start from n-1 and have bought the stock before and now must sell it.
+// above values are for t transactions left.
