ba5ed2f6b2a25a976cdad9f6437a17ec9d99f856
Shorya Dixit <98959122+Shorya-Dixit@users.noreply.github.com>
2024-10-06 22:36:59 +0530

    Time: 2 ms (48.62%), Space: 8.4 MB (34.35%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        
-        //Handling corner cases
+    int divide(int dividend, int divisor) {   
         if(dividend==INT_MIN && divisor==-1) return INT_MAX;
-        if(dividend==INT_MIN && divisor==1) return INT_MIN;
-
-        
+        if(dividend==INT_MIN && divisor==1) return INT_MIN;   
         //Converting divisors and dividend to their positive values
-        long int dd = abs(dividend), dv = abs(divisor);
-        
-        //Result Variables
+        long int dd = abs(dividend), dv = abs(divisor);   
         int res=0;
         while(dv<=dd) {
             long int mul=dv, tmp=1;
@@ -20,12 +14,8 @@ public:
             }
             res+=tmp;
             dd-=mul;
-        }
-        
-        //If either of dividend or divisor is negative our result will be negative
-        if((dividend<0&&divisor>0) || (dividend>0&&divisor<0)) return -res;
-        
-        return res;
-        
+        }    
+        if((dividend<0&&divisor>0) || (dividend>0&&divisor<0)) return -res;    
+        return res;     
     }
 };
